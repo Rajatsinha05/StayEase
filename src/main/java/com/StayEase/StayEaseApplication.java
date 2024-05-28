5276d3a9c8640886cd90184d8d1b4d2b9e450084
Rajat Sinha <111138210+Rajatsinha05@users.noreply.github.com>
2024-05-28 13:24:34 +0530

    setup

---
+++ src/main/java/com/StayEase/StayEaseApplication.java
@@ -0,0 +1,13 @@
+package com.StayEase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayEaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StayEaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/StayEase/StayEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StayEase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayEaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
